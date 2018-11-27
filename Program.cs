0242e0c9fc0f6c031156c02b0b62e987d4a352e1
Gregory Smith <gregsmith446@gmail.com>
2018-11-27 18:11:52 -0500

    basic dotnet program that uses string builder

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text;
+
+namespace cSharp_Fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder buildAString = new StringBuilder("Hellllllooooo World!!!");
+            Console.WriteLine(buildAString);
+        }
+    }
+}
